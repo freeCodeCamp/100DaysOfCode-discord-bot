ed498dab90456f82cd491766ad777a8b1eeec39e
Nicholas Carrigan (he/him) <nhcarrigan@gmail.com>
2021-09-08 09:56:41 -0700

    feat: install typescript (#71)

--- src/utils/errorHandler.ts
+++ src/utils/errorHandler.ts
@@ -1,6 +1,7 @@
 import * as Sentry from "@sentry/node";
 import { logHandler } from "./logHandler";
-export const errorHandler = (context: string, error: Error): void => {
+export const errorHandler = (context: string, err: unknown): void => {
+  const error = err as Error;
   logHandler.log("error", `There was an error in the ${context}:`);
   logHandler.log(
     "error",
