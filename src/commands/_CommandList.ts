261ab56246002a93f8117b1a5a7c03e227c459bf
Naomi Carrigan <nhcarrigan@gmail.com>
2022-05-24 20:03:24 -0700

    feat: add reset command

--- src/commands/_CommandList.ts
+++ src/commands/_CommandList.ts
@@ -2,6 +2,7 @@ import { CommandInt } from "../interfaces/CommandInt";
 import { edit } from "./edit";
 import { help } from "./help";
 import { oneHundred } from "./oneHundred";
+import { reset } from "./reset";
 import { view } from "./view";
 
-export const CommandList: CommandInt[] = [oneHundred, view, help, edit];
+export const CommandList: CommandInt[] = [oneHundred, view, help, edit, reset];
